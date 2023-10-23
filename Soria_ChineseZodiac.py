680268ae8f5bb8aa384d9eb4e11c15d1b0bbdfa0
Tesoi25 <soria.glenn1995@gmail.com>
2023-10-23 22:21:04 +0800

    October 23 2023 Activity

---
+++ Soria_ChineseZodiac.py
@@ -0,0 +1,29 @@
+year = int(input("Please input your birth year:"))
+
+if year == 1924  or year == 1936 or year == 1948 or year == 1960 or year == 1972 or year == 1984 or year == 1996 or year == 2008 or year == 2020 or year == 2032:
+    print("Your horoscope is Rat")
+elif year == 1925 or year == 1937 or year == 1949 or year == 1961 or year == 1973 or year == 1985 or year == 1997 or year == 2009 or year == 2021 or year == 2033:
+    print("Your horoscope is Ox")
+elif year == 1926 or year == 1938 or year == 1950 or year == 1962 or year == 1974 or year == 1986 or year == 1998 or year == 2010 or year == 2022 or year == 2034:
+    print("Your horoscope is Tiger")
+elif year == 1927 or year == 1939 or year == 1951 or year == 1961 or year == 1975 or year == 1987 or year == 1999 or year == 2011 or year == 2023 or year == 2035:
+    print("Your horoscope is Rabbit")
+elif year == 1928 or year == 1940 or year == 1952 or year == 1961 or year == 1976 or year == 1988 or year == 2000 or year == 2012 or year == 2024 or year == 2036:
+    print("Your horoscope is Dragon")
+elif year == 1929 or year == 1941 or year == 1953 or year == 1962 or year == 1977 or year == 1989 or year == 2001 or year == 2013 or year == 2025 or year == 2037:
+    print("Your horoscope is Snake")
+elif year == 1930 or year == 1942 or year == 1954 or year == 1963 or year == 1978 or year == 1990 or year == 2002 or year == 2014 or year == 2026 or year == 2038:
+    print("Your horoscope is Horse")
+elif year == 1931 or year == 1943 or year == 1955 or year == 1964 or year == 1979 or year == 1991 or year == 2003 or year == 2015 or year == 2027 or year == 2039:
+    print("Your horoscope is Sheep")
+elif year == 1932 or year == 1944 or year == 1956 or year == 1965 or year == 1980 or year == 1992 or year == 2004 or year == 2016 or year == 2028 or year == 2040:
+    print("Your horoscope is Monkey")
+elif year == 1933 or year == 1945 or year == 1957 or year == 1966 or year == 1981 or year == 1993 or year == 2005 or year == 2017 or year == 2029 or year == 2041:
+    print("Your horoscope is Rooster")
+elif year == 1934 or year == 1946 or year == 1958 or year == 1967 or year == 1982 or year == 1994 or year == 2006 or year == 2018 or year == 2030 or year == 2042:
+    print("Your horoscope is Dog")
+elif year == 1935 or year == 1947 or year == 1959 or year == 1968 or year == 1983 or year == 1995 or year == 2007 or year == 2019 or year == 2031 or year == 2043:
+    print("Your horoscope is Pig")
+    
+else:
+    print("Please input only 1924 to 2043 years")
